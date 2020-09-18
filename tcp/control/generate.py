a54465c1e79040f53a7c7dd83aaab2eec7beda1c
Vlad Grigorescu <vlad@es.net>
2020-09-18 10:55:20 -0500

    Add TCP examples for various flag combinations

--- tcp/control/generate.py
+++ tcp/control/generate.py
@@ -39,12 +39,23 @@ for orig_flags in all_flag_combos:
     orig_pkt = Ether()/IP(src=src_ip, dst=dest_ip)/TCP(sport=port,dport=dest_port,flags="".join(orig_flags))
     one_way_pkts.append(orig_pkt)
 
+    o = "".join(orig_flags)
+    if not o:
+        o = "None"
+    wrpcap("one_way_" + o + ".pcap", [orig_pkt])
+
     for resp_flags in all_flag_combos:
         port = get_random_port()
         orig_pkt = Ether()/IP(src=src_ip, dst=dest_ip)/TCP(sport=port,dport=dest_port,flags="".join(orig_flags))
         two_way_pkts.append(orig_pkt)
         resp_pkt = Ether()/IP(src=dest_ip, dst=src_ip)/TCP(sport=dest_port,dport=port,flags="".join(resp_flags))
         two_way_pkts.append(resp_pkt)
+
+        r = "".join(resp_flags)
+        if not r:
+            r = "None"
+
+        wrpcap("two_way_" + o + "_" + r + ".pcap", [orig_pkt, resp_pkt])
 
 wrpcap('one_way_all_flags.pcap', one_way_pkts)
 wrpcap('two_way_all_flags.pcap', two_way_pkts)
